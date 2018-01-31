a005e65512993f0b8d88334933c96454e9ed7321
Crash_hp Home <crashhp07@gmail.com>
2018-01-31 22:52:04 +0500

    Just Edit

--- MyRockingApp/Program.cs
+++ MyRockingApp/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyRockingApp
 {
@@ -10,6 +6,14 @@ namespace MyRockingApp
     {
         static void Main(string[] args)
         {
+            Console.Title = "My Rocking App";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("*************************************");
+            Console.WriteLine("***** Welcome to My Rocking App *****");
+            Console.WriteLine("*************************************");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ReadLine();
         }
     }
 }
