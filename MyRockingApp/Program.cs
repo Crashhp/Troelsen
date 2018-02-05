4a8753a289da9008c8005f914e2454d4a6ba54e3
Crash_hp <Crashhp07@gmail.com>
2018-02-05 14:20:17 +0500

    Class Diagram

--- MyRockingApp/Program.cs
+++ MyRockingApp/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Windows.Forms;
 
 namespace MyRockingApp
 {
     class Program
     {
+        public static SportsCar MyCar { get; set; }
+
         static void Main(string[] args)
         {
+            MyCar = new SportsCar();
             Console.Title = "My Rocking App";
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.BackgroundColor = ConsoleColor.Blue;
@@ -13,6 +17,7 @@ namespace MyRockingApp
             Console.WriteLine("***** Welcome to My Rocking App *****");
             Console.WriteLine("*************************************");
             Console.BackgroundColor = ConsoleColor.Black;
+            MessageBox.Show("My car name is " + MyCar.GetPetName());
             Console.ReadLine();
         }
     }
